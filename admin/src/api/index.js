d2c220ef605901414ebf044a1e5061bcfd5cdbbb
Loc DEV <locdev2000@gmail.com>
2021-12-06 20:30:12 +0700

    deploy

--- admin/src/api/index.js
+++ admin/src/api/index.js
@@ -1,6 +1,7 @@
 const api =
     process.env.NODE_ENV !== "production" ?
-    "http://localhost:5000" :
-    "someDeployURL";
+    "https://quanlysinhvien-app.herokuapp.com/" :
+    "http://localhost:5000" ;
+ ;
 
 export default api;
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,11 @@
+// const api =
+//     process.env.NODE_ENV !== "production" ?
+//     "http://localhost:5000" :
+//     "https://quanlysinhvien-app.herokuapp.com/";
 const api =
     process.env.NODE_ENV !== "production" ?
-    "http://localhost:5000" :
-    "someDeployURL";
+    "https://quanlysinhvien-app.herokuapp.com" :
+    "http://localhost:5000" ;
+ ;
 
 export default api;
